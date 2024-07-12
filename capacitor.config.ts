e5f835e4bc497b6adf677191466d2a4abd85ebc6
Jack Fitzgibbon <jackfitzgibbon@outlook.com>
2024-07-12 16:59:38 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MyBullion-App',
+  webDir: 'dist'
+};
+
+export default config;
